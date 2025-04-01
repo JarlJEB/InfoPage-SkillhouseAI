f12fbcb2ef6c297053654b845ab96acfca34cfa6
JarlJEB <julian@skillhouse.no>
2025-04-01 14:22:25 +0200

    v2-fixed: Fikset plassering, fontstørrelse og svarvisning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        skillgreen: "#2bb77b",
+        skillwhite: "#fcfcf2"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
