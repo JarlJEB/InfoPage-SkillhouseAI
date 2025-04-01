9c0736b704195630160cd57025500290d0f503be
JarlJEB <julian@skillhouse.no>
2025-04-01 14:19:25 +0200

    Versjon 2: Fontjustering, posisjonsfikser og logikkforbedring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        skillgreen: "#2bb77b",
-        skillwhite: "#fcfcf2"
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
